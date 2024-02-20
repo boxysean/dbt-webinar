1a8d4f86808c77fa341a7bc9a99bbd1257f7133b
Sean McIntyre <sean.mcintyre@fishtownanalytics.com>
2024-02-20 11:05:31 +0000

    Commit

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,12 +1,16 @@
-SELECT
-  orders_hub.O_ORDERKEY,
-  COUNT(nl.L_QUANTITY) as part_count,
-  SUM(nl.L_DISCOUNT) as total_discount,
-  SUM(nl.L_TAX) as total_tax,
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select
+  orders_hub.O_ORDERKEY as order_key,
+  count(nl.L_QUANTITY) as part_count,
+  sum(nl.L_DISCOUNT) as total_discount,
+  sum(nl.L_TAX) as total_tax,
   sum(nl.L_EXTENDEDPRICE) as price
-FROM {{ ref('LINEITEM_NL') }} as nl
-LEFT JOIN {{ ref('ORDERS_H') }} as orders_hub
-  ON nl.hk_order_h = orders_hub.hk_order_h
-LEFT JOIN {{ ref('ORDERS_N_S') }} as orders_sat
-  ON orders_hub.HK_ORDER_H = orders_sat.HK_ORDER_H
+from {{ ref('LINEITEM_NL') }} as nl
+left join {{ ref('ORDERS_H') }} as orders_hub on nl.HK_ORDER_H = orders_hub.hk_order_h
+left join {{ ref('ORDERS_N_S') }} as orders_sat on orders_hub.HK_ORDER_H = orders_sat.HK_ORDER_H
 group by 1
